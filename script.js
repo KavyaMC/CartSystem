2b3ac3759aafece4e51b8a50df8e9e8186072bc4
KavyaMC <270106151+KavyaMC@users.noreply.github.com>
2026-07-04 19:31:56 +0530

    feat: create application layout

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateTotal(amount)
+function createCartItem(name, price)
+function handleAddClick(e)
+function setupProductListeners()
+function handleCartClick(e)
+function handlePromoInput(e)
+function handleCheckout(e)
+function bootSystem()
+document.addEventListener("DOMContentLoaded", bootSystem);
